27434af3d7a8d4119d2f02e92690b59249e87e86
XCaminhante <caminhante@i2pmail.org>
2022-04-09 22:17:52 -0300

    Instagram page changed again, the index was selecting the previous item

--- instagram-dl.user.js
+++ instagram-dl.user.js
@@ -299,7 +299,7 @@
             if (!articleNode.querySelector('.coreSpriteLeftChevron')) {
                 idx = 0;
             } else if (!articleNode.querySelector('.coreSpriteRightChevron')) {
-                idx = list.length - 1;
+                idx = list.length - 2;
             } else idx = 1;
 
             let node = list[idx];
